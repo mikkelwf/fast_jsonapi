4e110f310dbae03d710e820f6ecdc80ddd436372
Mikkel Wied Frederiksen <mikkel@wied.cc>
2020-04-14 14:36:50 +0200

    User cache_key_with_version

--- lib/fast_jsonapi/serialization_core.rb
+++ lib/fast_jsonapi/serialization_core.rb
@@ -64,7 +64,7 @@ module FastJsonapi
 
       def record_hash(record, fieldset, params = {})
         if cached
-          record_hash = Rails.cache.fetch(record.cache_key, expires_in: cache_length, race_condition_ttl: race_condition_ttl) do
+          record_hash = Rails.cache.fetch(record.cache_key_with_version, expires_in: cache_length, race_condition_ttl: race_condition_ttl) do
             temp_hash = id_hash(id_from_record(record), record_type, true)
             temp_hash[:attributes] = attributes_hash(record, fieldset, params) if attributes_to_serialize.present?
             temp_hash[:relationships] = {}
